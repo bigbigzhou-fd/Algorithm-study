2e37a88476e9b51a92c5f94b201d9f0be88e1422
admin <20210240114@fudan.edu.cn>
2020-11-01 13:03:40 +0800

    2020/10/30/easy

---
+++ src/com01/Assignment4.java
@@ -0,0 +1,16 @@
+package com01;
+
+
+/**
+ * @Author zhouzhou
+ * @program: Algorithm
+ * @create: 2020-10-28 11:42
+ * @description
+ */
+
+
+public class Assignment4 {
+    public static void main(String[] args) {
+
+    }
+}
